a39908fa997dceeddb12ec989167bf7d05fe6720
Sergei Vorfolomeev <vorfo1897@gmail.com>
2022-12-06 16:41:10 +0100

    1 stage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './Header.module.css'
+import logo from '../img/logo.jpg'
 
 
 export const Header = () => {
     return (
         <header className={styles.header}>
+            <div className={styles.logoBox}>
+                <img src={logo} alt="logo" className={styles.logo}/>
+            </div>
             <div className={styles.textBox}>
                 <span className={styles.titlePrimary}>Orbita</span>
                 <span className={styles.titleSub}>Deeper into space</span>
             </div>
+
         </header>
     );
 };
