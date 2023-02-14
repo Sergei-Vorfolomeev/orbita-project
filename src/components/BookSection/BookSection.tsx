fceeaa4797b370f9b98b71be640b7105d1b3c7bc
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-02-14 16:45:43 +0100

    book section - step 3

--- src/components/BookSection/BookSection.tsx
+++ src/components/BookSection/BookSection.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Title} from "../common/Title";
+import {Button} from "../common/Button";
 
 export const BookSection = () => {
 
     const titleStyleMarginBottomMedium = {
         marginBottom: '70px'
+    }
+
+    const buttonStyle = {
+        backgroundColor: '#710D7DD1',
+        color: '#FFF'
     }
 
     return (
@@ -14,14 +20,35 @@ export const BookSection = () => {
                     <Title text={'Start booking now'} style={titleStyleMarginBottomMedium}/>
                     <form action="#" className={'form'}>
                         <div className="form__group">
-                            <input type="text" className={'form__input'} placeholder={'Full Name'} id={'name'}required/>
-                            <label htmlFor={'name'}>Full name</label>
+                            <input type="text" className={'form__input'} placeholder={'Full Name'} id={'name'}
+                                   required/>
+                            <label htmlFor={'name'} className={'form__label'}>Full name</label>
                         </div>
                         <div className="form__group">
-                            <input type="email" className={'form__input'} placeholder={'Email address'} id={'email'}required/>
-                            <label htmlFor={'email'}>Email address</label>
+                            <input type="email" className={'form__input'} placeholder={'Email address'} id={'email'}
+                                   required/>
+                            <label htmlFor={'email'} className={'form__label'}>Email address</label>
+                        </div>
+                        <div className="form__group">
+                            <div className="form__radio-group">
+                                <input type="radio" className="form__radio-input" id={'beginner'} name={'radio'}/>
+                                <label htmlFor="beginner" className={'form__radio-label'}>
+                                    <span className="form__radio-button"></span>
+                                    I'm beginner
+                                </label>
+                            </div>
+                            <div className="form__radio-group">
+                                <input type="radio" className="form__radio-input" id={'experienced'} name={'radio'}/>
+                                <label htmlFor="experienced" className={'form__radio-label'}>
+                                    <span className="form__radio-button"></span>
+                                    I'm experienced
+                                </label>
+                            </div>
                         </div>
                     </form>
+                    <div className={'button-box'}>
+                        <Button name={'Next step'} style={buttonStyle}/>
+                    </div>
                 </div>
             </div>
         </section>
