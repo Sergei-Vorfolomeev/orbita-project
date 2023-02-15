daa6a57a5badd2161c2920b01655e98389eb0747
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-02-15 19:54:09 +0100

    navigation - step 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,12 @@ import './App.css';
 import {Header} from "./components/Header";
 import {Main} from "./components/Main";
 import {Footer} from "./components/Footer";
-import {useState} from "react";
 import {Navigation} from "./components/Navigation";
 
 function App() {
-
-    const [navPage, setNavPage] = useState(false)
-
-    const onClickHandler = () => {
-        setNavPage(!navPage)
-    }
-
     return (
-        navPage
-            ? <Navigation navPage={navPage} setNavPage={setNavPage}/>
-            :
             <div className="App">
-                <div className="navigation-button" onClick={onClickHandler}></div>
+                <Navigation/>
                 <Header/>
                 <Main/>
                 <Footer/>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 
-type NavigationPropsType = {
-    navPage: boolean;
-    setNavPage: (value: boolean) => void
-}
-
-export const Navigation = ({navPage, setNavPage}: NavigationPropsType) => {
-    const onClickHandler = () => {
-        setNavPage(!navPage)
-    }
+export const Navigation = () => {
     return (
         <div className={'navigation'}>
-            <div className="navigation-button" onClick={onClickHandler}></div>
-
+            <input type="checkbox" className="navigation__checkbox" id={'navi-toggle'}/>
+            <label htmlFor="navi-toggle" className={'navigation__button'}></label>
+            <div className="navigation__background"></div>
+            <nav className="navigation__nav">
+                <ul className="navigation__list">
+                    <li className="navigation__item"><a href="#" className="navigation__link"><span>01</span>About Orbita</a></li>
+                    <li className="navigation__item"><a href="#" className="navigation__link"><span>02</span>Your benefits</a></li>
+                    <li className="navigation__item"><a href="#" className="navigation__link"><span>03</span>Popular tours</a></li>
+                    <li className="navigation__item"><a href="#" className="navigation__link"><span>04</span>Stories</a></li>
+                    <li className="navigation__item"><a href="#" className="navigation__link"><span>05</span>Book now</a></li>
+                </ul>
+            </nav>
         </div>
     );
 };
