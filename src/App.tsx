ec05ecb30b8a250bdd72345e92fdce84bd1c5a70
Sergei Vorfolomeev <vorfo1897@gmail.com>
2023-04-04 01:46:04 +0200

    feat:loading

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,26 @@ import {Header} from "./components/Header";
 import {Main} from "./components/Main";
 import {Footer} from "./components/Footer";
 import {Navigation} from "./components/Navigation";
+import {useEffect, useState} from "react";
+import {Loader} from "./components/Loader/Loader";
 
 function App() {
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(true)
+        }, 1000)
+    },[])
+
+    if (!loading) return <Loader/>
     return (
-            <div className="App">
-                <Navigation/>
-                <Header/>
-                <Main/>
-                <Footer/>
-            </div>
+        <div className="App">
+            <Navigation/>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
     );
 }
 
